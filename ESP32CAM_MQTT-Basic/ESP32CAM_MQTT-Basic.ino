e1d390ad055b1c7a4f476c3317240b0a9ce8d08b
victor <monsivaiss@hotmail.com>
2022-10-04 16:06:44 -0500

    Update ESP32CAM_MQTT-Basic.ino

--- ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
+++ ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
@@ -23,7 +23,7 @@ const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu re
 
 //Datos del broker MQTT
 const char* mqtt_server = "192.168.15.33"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
-IPAddress server(192,168,15,33);
+IPAddress server(192,168,15,33);	
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
